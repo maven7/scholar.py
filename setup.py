1d99a27c474e11fcc78d6e3e94c248c63e4302bb
David Takacs <david.takacs@maven7.com>
2016-06-27 12:35:42 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Maven7 scholar.py fork",
+    version="0.0.1",
+    packages=find_packages(),
+    author_email="david.takacs@maven7.com",
+    install_requires=[]
+)
